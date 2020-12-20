088fc790b9c4244dcbf81744629eee71acdd3535
maximelarrieu <maxime.larrieu@ynov.com>
2020-12-20 19:04:02 +0100

    :zap: add navbar function

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CommodityRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,12 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(CommodityRepository $commodityRepository): Response
     {
-
-        return $this->render('home/index.html.twig');
+        $commodities = $commodityRepository->findAll();
+        return $this->render('home/index.html.twig', [
+            'commodities' => $commodities
+        ]);
     }
 
     public function navbar(): Response {
