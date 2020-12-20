e588f7ef777f639f03a7b62312c7ce604bd9ce95
maximelarrieu <maxime.larrieu@ynov.com>
2020-12-20 04:16:17 +0100

    :wrench: business css entry

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
-
+    .addEntry('business-card', './assets/styles/business/business-card.css')
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
 
