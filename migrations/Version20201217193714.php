404e147150756331da5ec58ce4c6c64336e41b51
maximelarrieu <maxime.larrieu@ynov.com>
2020-12-20 19:04:36 +0100

    :zap: change all int to float

--- migrations/Version20201217193714.php
+++ migrations/Version20201217193714.php
@@ -20,7 +20,7 @@ final class Version20201217193714 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE commodity (id INT AUTO_INCREMENT NOT NULL, business_id INT NOT NULL, title VARCHAR(60) NOT NULL, description LONGTEXT NOT NULL, image VARCHAR(100) DEFAULT NULL, price INT NOT NULL, remaining INT NOT NULL, INDEX IDX_5E8D2F74A89DB457 (business_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE commodity (id INT AUTO_INCREMENT NOT NULL, business_id INT NOT NULL, title VARCHAR(60) NOT NULL, description LONGTEXT NOT NULL, image VARCHAR(100) DEFAULT NULL, price FLOAT NOT NULL, remaining INT NOT NULL, INDEX IDX_5E8D2F74A89DB457 (business_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE commodity ADD CONSTRAINT FK_5E8D2F74A89DB457 FOREIGN KEY (business_id) REFERENCES business (id)');
     }
 
