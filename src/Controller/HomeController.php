cf07b1b697b54e3a311d001de49a0513e332ecc7
maximelarrieu <maxime.larrieu@ynov.com>
2020-12-20 04:16:47 +0100

    :sparkles: add navbar function

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,8 +13,15 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+
+        return $this->render('home/index.html.twig');
+    }
+
+    public function navbar(): Response {
+        $user = $this->getUser();
+
+        return $this->render('navbar.html.twig', [
+            'user' => $user
         ]);
     }
 }
