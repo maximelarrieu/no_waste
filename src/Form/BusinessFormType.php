b9656829dfdbec5f37f452127446101bd1fd7c08
maximelarrieu <maxime.larrieu@ynov.com>
2020-12-28 14:44:12 +0100

    :zap: replace imagefield by vichimagefield

--- src/Form/BusinessFormType.php
+++ src/Form/BusinessFormType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class BusinessFormType extends AbstractType
 {
@@ -20,9 +21,15 @@ class BusinessFormType extends AbstractType
             ->add('description', TextareaType::class, [
                 'label' => "Description :"
             ])
-            ->add('image', TextType::class, [
+            ->add('imageFile', VichImageType::class, [
                 'label' => "Image :",
                 'required' => false,
+                'allow_delete' => true,
+                'delete_label' => "Supprimer l'image",
+                'download_uri' => true,
+                'download_label' => "Télécharger l'image",
+                'image_uri' => true,
+                'asset_helper' => true,
             ])
             ->add('address', TextType::class, [
                 'label' => "Adresse postale :",
--- src/Form/CommodityType.php
+++ src/Form/CommodityType.php
@@ -10,6 +10,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class CommodityType extends AbstractType
 {
@@ -22,9 +23,15 @@ class CommodityType extends AbstractType
             ->add('description', TextareaType::class, [
                 'label' => "Description :"
             ])
-            ->add('image', TextType::class, [
+            ->add('imageFile', VichImageType::class, [
                 'label' => "Image :",
-                'required' => false
+                'required' => false,
+                'allow_delete' => true,
+                'delete_label' => "Supprimer l'image",
+                'download_uri' => true,
+                'download_label' => "Télécharger l'image",
+                'image_uri' => true,
+                'asset_helper' => true,
             ])
             ->add('price', MoneyType::class, [
                 'label' => "Prix du prduit :"
